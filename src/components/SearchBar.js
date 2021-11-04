817f8439c353b335b399ab8fd449fc168467d277
Jeff Kimball <jeffkimballis@gmail.com>
2021-11-03 22:14:15 -0500

    set up controlled form

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state={ term : ''}
+    
+    onInputChange = (e) => {
+        this.setState({term: e.target.value})
+        console.log(this.state.term)
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault()
+
+        
+    }
+
+    render(){
+        return(
+            <div className="ui segment search-bar">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input 
+                            value={this.state.term} 
+                            onChange={this.onInputChange}
+                            type="text"/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default SearchBar
